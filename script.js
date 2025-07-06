59af9ee018e3636ef27f358f568b5e92f2c627e1
Artystycznepaznokcie <bartlomiej.sokalski.job@gmail.com>
2025-07-06 13:29:53 +0200

    pierwszy kod js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+const button = document.querySelector(".js-button");
+const doneList = document.getElementById("doneList");
+
+button.onclick = () => {
+  doneList.style.display = "block";
+  button.style.display = "none";
+};
