a1fb829528fed369b91949d4f759c4783dc7a0cd
Artystycznepaznokcie <bartlomiej.sokalski.job@gmail.com>
2025-07-07 15:49:17 +0200

    zadanie 2

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-function shortenName(person) {
-  let findSpace = person.indexOf(" ");
-  if (findSpace === -1) {
-    return person;
-  } else if (findSpace > 0) {
-    let shortSurrname = findSpace + 2;
-    let cutSurrname = person.slice(0, shortSurrname);
-    let dot = ".";
+// function shortenName(person) {
+//   let findSpace = person.indexOf(" ");
+//   if (findSpace === -1) {
+//     return person;
+//   } else if (findSpace > 0) {
+//     let shortSurrname = findSpace + 2;
+//     let cutSurrname = person.slice(0, shortSurrname);
+//     let dot = ".";
 
-    let allTogether = cutSurrname + dot;
-    return allTogether;
+//     let allTogether = cutSurrname + dot;
+//     return allTogether;
+//   }
+// }
+// console.log(shortenName("kuba kamilicki"));
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function passwordStrength(password) {
+  if (password.length > 10 && password.match(/[!@#$%^&*]/)) {
+    return "hasło jest silne";
+  } else if (password.length > 6 && password.match(/\d/)) {
+    return "hasło jest medium";
+  } else if (password.length <= 6) {
+    return "masz weak hasło";
+  } else {
+    return undefined;
   }
 }
-console.log(shortenName("kuba kamilicki"));
+console.log(passwordStrength("Messi"));
